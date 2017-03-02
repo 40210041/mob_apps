62a6ccddd4cb0dcab6b8341fdfcac7b68a4497ab
40210041 <40210041@live.napier.ac.uk>
2017-03-02 14:48:07 +0000

    updated layout

--- DecisionMaker/app/src/main/java/com/example/mandles/decisionmaker/MainMenu.java
+++ DecisionMaker/app/src/main/java/com/example/mandles/decisionmaker/MainMenu.java
@@ -1,5 +1,6 @@
 package com.example.mandles.decisionmaker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
@@ -7,11 +8,48 @@ import android.widget.ImageView;
 import android.widget.Button;
 import android.view.View;
 
+import static com.example.mandles.decisionmaker.R.id.activity_rps;
+
 public class MainMenu extends AppCompatActivity {
+
+    Button btn_rps;
+    Button btn_coin_toss;
+    Button btn_dice_roll;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        btn_rps = (Button) findViewById(R.id.btn_rps);
+        btn_coin_toss = (Button) findViewById(R.id.btn_coin_toss);
+        btn_dice_roll = (Button) findViewById(R.id.btn_dice_roll);
+
+        btn_rps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Intent activity_main_menu = new Intent(MainMenu.this, RPS.class);
+                startActivity(activity_main_menu);
+            }
+        });
+
+        btn_coin_toss.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Intent activity_main_menu = new Intent(MainMenu.this, CoinToss.class);
+                startActivity(activity_main_menu);
+            }
+        });
+
+        btn_dice_roll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Intent activity_main_menu = new Intent(MainMenu.this, DiceRoll.class);
+                startActivity(activity_main_menu);
+            }
+        });
     }
 }
